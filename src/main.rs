a705a09b3a1aa17e0ab9511eaa866e007d4a002d
Bryant.Tseng <work.bryant.tseng@gmail.com>
2024-07-05 13:25:38 +0900

    fix fugle token

--- src/main.rs
+++ src/main.rs
@@ -9,7 +9,7 @@ use utils::config::Config;
 
 #[tokio::main]
 async fn main() {
-    let config = Config::Load();
+    let config = Config::load();
     let mut discord = Discord::new(config.clone()).await;
     discord.start().await;
 }
